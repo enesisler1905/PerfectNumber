5bdf36bff73fbfc15870bb5fe73df14fe731ae61
enesisler1905 <58097246+enesisler1905@users.noreply.github.com>
2022-07-12 21:23:23 +0300

    co

--- perfectNumber/ViewController.swift
+++ perfectNumber/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NumberTextField: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func ClickedButton(_ sender: Any) {
+        var _sum = 0
+        let values = NumberTextField.text!
+        let _x : Int! = Int(values)
+        
+        for i in 1..._x-1 {
+            if(_x % i == 0){
+                _sum = _sum + i
+                
+            }
+        }
+        if(_sum == _x ){
+            Label.text="Perfect Number"
+        }
+        else{
+            Label.text="Not Perfect Number"
+        }
+        
+        
+        
+    }
+    
 }
 
